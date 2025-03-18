e15f968724f1de3514442d9b66455b90a3296e1c
Krishna Gopal <kgopal492@gmail.com>
2025-03-18 13:05:13 -0700

    fix(tooltip): Remove percents totals (#51)
    * fix: Time-series Line Chart Display unnecessary total (#31181)
    
    * default to false for showTotal/showPercent
    
    ---------
    
    Co-authored-by: Michael S. Molina <70410625+michael-s-molina@users.noreply.github.com>

--- superset-frontend/plugins/plugin-chart-echarts/src/Timeseries/transformProps.ts
+++ superset-frontend/plugins/plugin-chart-echarts/src/Timeseries/transformProps.ts
@@ -170,6 +170,8 @@ export default function transformProps(
     stack,
     tooltipTimeFormat,
     tooltipSortByMetric,
+    showTooltipTotal,
+    showTooltipPercentage,
     truncateXAxis,
     truncateYAxis,
     xAxis: xAxisOrig,
@@ -192,7 +194,9 @@ export default function transformProps(
   }: EchartsTimeseriesFormData = { ...DEFAULT_FORM_DATA, ...formData };
   const refs: Refs = {};
   const groupBy = ensureIsArray(groupby);
-  const labelMap = Object.entries(label_map).reduce((acc, entry) => {
+  const labelMap: { [key: string]: string[] } = Object.entries(
+    label_map,
+  ).reduce((acc, entry) => {
     if (
       entry[1].length > groupBy.length &&
       Array.isArray(timeCompare) &&
@@ -486,7 +490,9 @@ export default function transformProps(
     minorTick: { show: minorTicks },
     minInterval:
       xAxisType === AxisType.Time && timeGrainSqla
-        ? TIMEGRAIN_TO_TIMESTAMP[timeGrainSqla]
+        ? TIMEGRAIN_TO_TIMESTAMP[
+            timeGrainSqla as keyof typeof TIMEGRAIN_TO_TIMESTAMP
+          ]
         : 0,
     ...getMinAndMaxFromBounds(
       xAxisType,
@@ -574,9 +580,10 @@ export default function transformProps(
                 value.observation !== undefined ? acc + value.observation : acc,
               0,
             );
-            const showTotal =
+            const allowTotal =
               Boolean(isMultiSeries) && richTooltip && !isForecast;
-            const showPercentage = showTotal && !forcePercentFormatter;
+            const showPercentage =
+              allowTotal && !forcePercentFormatter && showTooltipPercentage;
             const keys = Object.keys(forecastValues);
             let focusedRow;
             sortedKeys
@@ -607,7 +614,7 @@ export default function transformProps(
                 focusedRow = rows.length - focusedRow - 1;
               }
             }
-            if (showTotal) {
+            if (allowTotal && showTooltipTotal) {
               const totalRow = ['Total', formatter.format(total)];
               if (showPercentage) {
                 totalRow.push(percentFormatter.format(1));
