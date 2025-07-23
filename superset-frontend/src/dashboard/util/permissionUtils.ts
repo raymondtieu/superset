ee2d96499cd960242565a2112358a005291f510f
Krishna Gopal <kgopal492@gmail.com>
2025-07-23 13:05:40 -0700

    Fix python & frontend test (#73)

--- superset-frontend/src/dashboard/util/permissionUtils.ts
+++ superset-frontend/src/dashboard/util/permissionUtils.ts
@@ -16,7 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-import { isFeatureEnabled, FeatureFlag } from '@superset-ui/core';
+// import { isFeatureEnabled, FeatureFlag } from '@superset-ui/core';
 import {
   isUserWithPermissionsAndRoles,
   UndefinedUser,
