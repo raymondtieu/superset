68fb999a3d08f6612703cf80d4c65fd4cfeb80e1
Raymond Tieu <8702151+raymondtieu@users.noreply.github.com>
2025-09-09 18:58:50 +0000

    extract event context from log messages coming from front-end

--- superset/utils/log.py
+++ superset/utils/log.py
@@ -178,6 +178,7 @@ class AbstractEventLogger(ABC):
         object_ref: str | None = None,
         log_to_statsd: bool = True,
         database: Any | None = None,
+        payload: dict[str, Any] | None = None,
         **payload_override: dict[str, Any] | None,
     ) -> None:
         # pylint: disable=import-outside-toplevel
@@ -201,7 +202,9 @@ class AbstractEventLogger(ABC):
                 logging.warning(ex)
                 user_id = None
 
-        payload = collect_request_payload()
+        if payload is None:
+            payload = collect_request_payload()
+
         if object_ref:
             payload["object_ref"] = object_ref
         if payload_override:
